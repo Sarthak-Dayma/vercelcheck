360cec338a84750e3c825234d4688be89e3fd548
Sarthak-Dayma <daymasarthak2@gmail.com>
2025-03-07 00:47:10 +0530

    git git

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/demo4",{
  app.use(bodyParser.json({extended :true}));
  app.use(bodyParser.urlencoded({extended :true}));
  app.use(cors());
-   app.use('/',(req, res) =>{});
-   res.json({message: "Hello from ExpressApp"})
+   app.use('/',router);
 
   app.listen(8001   ,()=> console.log("Server is running on port 8000"));
 
