a9ba71344f13b75409e4e0be91700e440cdcd598
Sarthak-Dayma <daymasarthak2@gmail.com>
2025-03-07 00:41:18 +0530

    package modify

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/demo4",{
  app.use(bodyParser.json({extended :true}));
  app.use(bodyParser.urlencoded({extended :true}));
  app.use(cors());
-   app.use('/',router);
+   app.use('/',(req, res) =>{});
+   res.json({message: "Hello from ExpressApp"})
 
   app.listen(8001   ,()=> console.log("Server is running on port 8000"));
 
